fcb7bf6326306281885b3c76b26af24e79adb0d1
kofdan <kofdan1993@gmail.com>
2022-04-12 18:55:09 +0300

    therd

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,7 +9,7 @@ function Card(props){
             <img src={props.imgPath} alt={props.title} />
             <h3>{props.title}</h3>
             <p>Стоимость: {props.price}</p>
-            <Button variant='contained' name={props.id}>Купить</Button>
+            <Button variant='contained' name={props.id}>Купить:</Button>
         </div>
         </>
     )
