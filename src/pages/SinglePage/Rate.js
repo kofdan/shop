e50635d5d66e54926921166eee54deaf677c3354
Vasilii Merlikov <96536616+kofdan@users.noreply.github.com>
2022-08-14 23:02:39 +0300

    23:02

---
+++ src/pages/SinglePage/Rate.js
@@ -0,0 +1,31 @@
+{
+  /* <div class="starRate">
+  <ul>
+    <li>
+      <a href="#" class="stars" data-value="5" style="">
+        <span>5 stars</span>
+      </a>
+    </li>
+    <li>
+      <a href="#" class="stars" data-value="4" style="">
+        <span>4 stars</span>
+      </a>
+    </li>
+    <li>
+      <a href="#" class="stars" data-value="3" style="">
+        <span>3 stars</span>
+      </a>
+    </li>
+    <li>
+      <a href="#" class="stars" data-value="2" style="">
+        <span>2 stars</span>
+      </a>
+    </li>
+    <li>
+      <a href="#" class="stars" data-value="1" style="">
+        <span>1 star</span>
+      </a>
+    </li>
+  </ul>
+</div>; */
+}
--- src/pages/SinglePage/ReviewsForm.js
+++ src/pages/SinglePage/ReviewsForm.js
@@ -1,17 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./ReviewsFormStyle.module.css";
 
 function ReviewsForm() {
   const [value, setValue] = useState("");
-  const auth = false;
+  const [revError, setRevError] = useState(false);
+  const [fieldValid, setFieldValid] = useState(false);
+  const [dataSend, setDataSend] = useState(false);
+
   const handleSubmit = (event) => {
     event.preventDefault();
+    setValue("");
+    setDataSend(true);
   };
+
   const handleBlur = () => {
     if (value === "") {
-      console.log(123);
+      setRevError(true);
+      setDataSend(false);
+    } else {
+      setRevError(false);
     }
   };
+  const handleFocus = (e) => dataSend && setDataSend(false);
+
+  const handleOnChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  useEffect(() => {
+    if (value.length > 0) {
+      setFieldValid(true);
+      setRevError(false);
+    } else {
+      setFieldValid(false);
+    }
+  }, [value, dataSend]);
+
   return (
     <>
       <form
@@ -19,15 +43,18 @@ function ReviewsForm() {
         onSubmit={handleSubmit}
         onBlur={handleBlur}
       >
-        <label>
-          Enter your name:
-          <input
-            type="text"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          />
-        </label>
-        <input type="submit" />
+        <textarea
+          onBlur={handleBlur}
+          onChange={handleOnChange}
+          onFocus={handleFocus}
+          id="content"
+          placeholder="Type your review..."
+        ></textarea>
+        <button disabled={!fieldValid}>Send</button>
+        {dataSend && <p className={styles.succsess}>Your message send. Thx</p>}
+        {revError && (
+          <p className={styles.error}>this field must be not empty</p>
+        )}
       </form>
     </>
   );
